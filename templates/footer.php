b0b03a588ced33a9302a1f132c6b27e267e559d0
kylan <kylan@datagoniaweb.com>
2015-10-07 20:06:11 -0700

    Adding examples, angular file, etc

---
+++ templates/footer.php
@@ -0,0 +1,12 @@
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->
+    <script src="https://code.jquery.com/jquery-2.1.4.js"></script>
+    <script src="docs/dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="docs/assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
