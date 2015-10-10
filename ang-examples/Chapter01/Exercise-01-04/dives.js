e4b0b88973c0cfef516548ff8b6ff008650cff89
kylan <kylan@datagoniaweb.com>
2015-10-10 00:01:20 -0700

    Example 01-04

--- ang-examples/Chapter01/Exercise-01-04/dives.js
+++ ang-examples/Chapter01/Exercise-01-04/dives.js
@@ -1,19 +1,3 @@
-﻿var dives = [
-  {
-    site: 'Abu Gotta Ramada',
-    location: 'Hurghada, Egypt',
-    depth: 72,
-    time: 54
-  },
-  {
-    site: 'Ponte Mahoon',
-    location: 'Maehbourg, Mauritius',
-    depth: 54,
-    time: 38
-  },
-  {
-    site: 'Molnar Cave',
-    location: 'Budapest, Hungary',
-    depth: 98,
-    time: 62
-  }];
+var employers = 
+        [{company: 'Z Global', location: 'La Jolla'},
+    {company: 'KnowledgeCity', location:'Carlsbad'}];
