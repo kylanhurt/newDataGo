a749a09273102ec5a69b1618576d55eee9d23e15
kylan <kylan@datagoniaweb.com>
2015-10-16 12:40:03 -0700

    Re-organization

---
+++ employers.js
@@ -0,0 +1,3 @@
+var employers =
+        [{company: 'Z Global', location: 'La Jolla', supervisor: 'Sean Flynn', position: 'Web Developer'},
+            {company: 'KnowledgeCity', location: 'Carlsbad', supervisor: 'James Morris', position:'Web Developer'}];
