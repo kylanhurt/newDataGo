945936db8b36edd1dc72e3eb7f0e84f83815ba05
Kylan Hurt <kylan@datagoniaweb.com>
2016-01-06 20:04:28 -0800

    Initial database tables created, migrations lesson completed

--- api/database/migrations/2016_01_07_034637_create_entities_table.php
+++ api/database/migrations/2016_01_07_034637_create_entities_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateEntitiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('entities', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('description');
             $table->timestamps();
+            $table->timestamp('published');
         });
     }
 
@@ -29,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('entities');
     }
 }
